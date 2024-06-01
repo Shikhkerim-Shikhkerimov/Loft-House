a0026d3545a6d4298d67ce53294abdf19fe69e78
Shikhkerimov Shikhkerim <kerim_shikhkerimov@mail.ru>
2024-06-01 23:29:02 +0400

    js code for the mobile version menu button

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// кнопка меню мобильной версии
+const navBtn = document.querySelector(".nav-icon-btn");
+const navIcon = document.querySelector(".nav-icon");
+const nav = document.querySelector(".header-section__top");
+
+navBtn.onclick = function () {
+  navIcon.classList.toggle("nav-icon--active");
+  nav.classList.toggle("header-section__top--mobile");
+  document.body.classList.toggle("no-scroll");
+};
